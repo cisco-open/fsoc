56108590582dfe90c1a3fba638dce350098c54c2
GDW1 <guwilks@cisco.com>
2023-02-25 18:38:23 -0800

    basic description of fields of solution - TODO verification of output format

--- cmd/solution/describe.go
+++ cmd/solution/describe.go
@@ -1,10 +1,12 @@
 package solution
 
 import (
+	"fmt"
 	"github.com/apex/log"
 	"github.com/cisco-open/fsoc/cmd/config"
-	"github.com/cisco-open/fsoc/cmdkit"
+	"github.com/cisco-open/fsoc/platform/api"
 	"github.com/spf13/cobra"
+	"net/url"
 )
 
 var solutionDescribeCmd = &cobra.Command{
@@ -12,6 +14,17 @@ var solutionDescribeCmd = &cobra.Command{
 	Short: "",
 	Long:  ``,
 	Run:   solutionDescribe,
+}
+
+type Solution struct {
+	ID             string `json:"id"`
+	LayerID        string `json:"layerId"`
+	LayerType      string `json:"layerType"`
+	ObjectMimeType string `json:"objectMimeType"`
+	TargetObjectId string `json:"targetObjectId"`
+	CreatedAt      string `json:"createdAt"`
+	UpdatedAt      string `json:"updatedAt"`
+	DisplayName    string `json:"displayName"`
 }
 
 func getSolutionDescribeCmd() *cobra.Command {
@@ -23,7 +36,8 @@ func getSolutionDescribeCmd() *cobra.Command {
 }
 
 func solutionDescribe(cmd *cobra.Command, args []string) {
-	log.Info("Fetching the list of solutions...")
+	log.Info("Fetching the details of the specified solutions...")
+	solution, _ := cmd.Flags().GetString("solution")
 
 	cfg := config.GetCurrentContext()
 	layerID := cfg.Tenant
@@ -33,10 +47,19 @@ func solutionDescribe(cmd *cobra.Command, args []string) {
 		"layer-id":   layerID,
 	}
 
-	// get data and display
-	cmdkit.FetchAndPrint(cmd, getSolutionDescribeUrl(), &cmdkit.FetchAndPrintOptions{Headers: headers, IsCollection: true})
+	log.Infof("Getting details of the '%s' solution", solution)
+	var res Solution
+	api.JSONGet(getSolutionDescribeUrl(url.PathEscape(solution)), &res, &api.Options{Headers: headers}) //(getSolutionDescribeUrl(), &api.Options{Headers: headers})
+	fmt.Printf("ID: %s\n", res.ID)
+	fmt.Printf("LayerID: %s\n", res.LayerID)
+	fmt.Printf("layerType: %s\n", res.LayerType)
+	fmt.Printf("ObjectMimeType: %s\n", res.ObjectMimeType)
+	fmt.Printf("TargetObjectId: %s\n", res.TargetObjectId)
+	fmt.Printf("CreatedAt: %s\n", res.CreatedAt)
+	fmt.Printf("UpdatedAt: %s\n", res.UpdatedAt)
 }
 
-func getSolutionDescribeUrl() string {
-	return "objstore/v1beta/objects/extensibility"
+func getSolutionDescribeUrl(id string) string {
+	//println(id)
+	return "objstore/v1beta/objects/extensibility:solution/" + id
 }
