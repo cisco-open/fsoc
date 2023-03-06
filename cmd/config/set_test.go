07b2bf7eeab99ab84f4ca0957745153b6b47774a
Will Li <cuichen.li@appdynamics.com>
2023-03-06 12:14:33 +0800

    update based on communication

---
+++ cmd/config/set_test.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidateUrlWithInvalidURL(t *testing.T) {
+	url, err := validateUrl("/web")
+	assert.Equal(t, "", url)
+	assert.NotNil(t, err)
+	assert.Regexp(t, "no host is provided in the url.*", err)
+
+	url, err = validateUrl("h://web")
+	assert.Equal(t, "", url)
+	assert.NotNil(t, err)
+	assert.Regexp(t, "invalid schema.*", err)
+}
+
+func TestValidateUrlWithDataNeedClean(t *testing.T) {
+	url, err := validateUrl("mytenant.saas.observe.com")
+	assert.Nil(t, err)
+	assert.Equal(t, url, "https://mytenant.saas.observe.com")
+
+	url, err = validateUrl("mytenant.saas.observe.com/index/a?b=1")
+	assert.Nil(t, err)
+	assert.Equal(t, url, "https://mytenant.saas.observe.com/index/a?b=1")
+}
+
+func TestValidateUrlWithValidData(t *testing.T) {
+	url, err := validateUrl("http://mytenant.saas.observe.com")
+	assert.Nil(t, err)
+	assert.Equal(t, url, "http://mytenant.saas.observe.com")
+}
