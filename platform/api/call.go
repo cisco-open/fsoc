f3cc2d19c833d68aeb9fd0bd8cff926599be4a0e
pnickolov <pnickolov@users.noreply.github.com>
2023-03-07 02:15:44 -0800

    Refactor platform api (#45)
    * maint: refactor call to remove duplication
    * maint: streamline error handling in platform/api

--- platform/api/call.go
+++ platform/api/call.go
@@ -39,57 +39,19 @@ type Options struct {
 	ResponseHeaders map[string][]string // headers as returned by the call
 }
 
-// Problem type is a json object returned for content-type application/problem+json according to the RFC-7807
-type Problem struct {
-	Type       string `json:"type"`
-	Title      string `json:"title"`
-	Detail     string `json:"detail"`
-	Status     int    `json:"status"`
-	Extensions map[string]any
-}
-
-func (p *Problem) UnmarshalJSON(bs []byte) (err error) {
-	type _Problem Problem
-	commonFields := _Problem{}
-
-	// If the commonFields was of unaliased type Problem, this method UnmarshalJSON would be called in recursion.
-	// When we try to unmarshall data to struct _Problem, the default behavior based on json tags on struct fields
-	// is used instead of this method.
-	if err = json.Unmarshal(bs, &commonFields); err == nil {
-		*p = Problem(commonFields)
-	}
-
-	extensions := make(map[string]interface{})
-
-	// in the second go of the unmarshalling we are parsing all undefined fields that may be part of the JSON object
-	if err = json.Unmarshal(bs, &extensions); err == nil {
-		delete(extensions, "type")
-		delete(extensions, "title")
-		delete(extensions, "detail")
-		delete(extensions, "status")
-		p.Extensions = extensions
-	}
-
-	return err
-}
-
-func (p Problem) Error() string {
-	return fmt.Sprintf("%s - %s", p.Title, p.Detail)
-}
-
 // JSONGet performs a GET request and parses the response as JSON
 func JSONGet(path string, out any, options *Options) error {
-	return jsonRequest("GET", path, nil, out, options)
+	return httpRequest("GET", path, nil, out, options)
 }
 
 // JSONDelete performs a DELETE request and parses the response as JSON
 func JSONDelete(path string, out any, options *Options) error {
-	return jsonRequest("DELETE", path, nil, out, options)
+	return httpRequest("DELETE", path, nil, out, options)
 }
 
 // JSONPost performs a POST request with JSON command and response
 func JSONPost(path string, body any, out any, options *Options) error {
-	return jsonRequest("POST", path, body, out, options)
+	return httpRequest("POST", path, body, out, options)
 }
 
 // HTTPPost performs a POST request with HTTP command and response - Accept and Content-Type headers are provided by the caller
@@ -104,193 +66,74 @@ func HTTPGet(path string, out any, options *Options) error {
 
 // JSONPut performs a PUT request with JSON command and response
 func JSONPut(path string, body any, out any, options *Options) error {
-	return jsonRequest("PUT", path, body, out, options)
+	return httpRequest("PUT", path, body, out, options)
 }
 
 // JSONPatch performs a PATCH request and parses the response as JSON
 func JSONPatch(path string, body any, out any, options *Options) error {
-	return jsonRequest("PATCH", path, body, out, options)
+	return httpRequest("PATCH", path, body, out, options)
 }
 
 // JSONRequest performs an HTTP request and parses the response as JSON, allowing
 // the http method to be specified
 func JSONRequest(method string, path string, body any, out any, options *Options) error {
-	return jsonRequest(method, path, body, out, options)
+	return httpRequest(method, path, body, out, options)
 }
 
 // --- Internal methods -----------------------------------------------------
 
-func jsonRequest(method string, path string, body any, out any, options *Options) error {
-	log.WithFields(log.Fields{"method": method, "path": path}).Info("Calling FSO platform API")
+func prepareHTTPRequest(cfg *config.Context, client *http.Client, method string, path string, body any, headers map[string]string) (*http.Request, error) {
+	// body will be JSONified if a body is given but no Content-Type is provided
+	// (if a content type is provided, we assume the body is in the desired format)
+	jsonify := body != nil && (headers == nil || headers["Content-Type"] == "")
 
-	// create a default options to avoid nil-checking
-	if options == nil {
-		options = &Options{}
-	}
-
-	// get current context to obtain the URL and token (TODO: consider supporting unauth access for local dev)
-	cfg := config.GetCurrentContext()
-	if cfg == nil {
-		return errors.New("Missing context; use 'fsoc config set' to configure your context")
-	}
-	log.WithFields(log.Fields{"context": cfg.Name, "url": cfg.URL, "tenant": cfg.Tenant}).Info("Using context")
-
-	// force login if no token
-	if cfg.Token == "" {
-		log.Infof("No token available, trying to log in")
-		if err := Login(); err != nil {
-			return err
-		}
-		cfg = config.GetCurrentContext()
-		if cfg.Token == "" {
-			return errors.New("Login succeeded but did not provide a token")
-		}
-	}
-
-	// create http client for the request
-	client := &http.Client{}
-
-	// build HTTP request
-	req, err := prepareJSONRequest(cfg, client, method, path, body, options.Headers)
-	if err != nil {
-		return err // anything that needed logging has been logged
-	}
-
-	// execute request
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("%v request to %q failed: %v", method, req.RequestURI, err)
-	}
-
-	// log error if it occurred
-	if resp.StatusCode/100 != 2 {
-		// log error before trying to parse body, more processing later
-		log.Errorf("Request failed, status %q; more info to follow", resp.Status)
-	}
-
-	// collect response body (whether success or error)
-	var respBytes []byte
-	defer resp.Body.Close()
-	respBytes, err = io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("Failed reading response to %v to %q: %v", method, req.RequestURI, err)
-	}
-
-	// handle special case when access token needs to be refreshed and request retried
-	if resp.StatusCode == http.StatusForbidden {
-		log.Info("Current token is no longer valid; trying to refresh")
-		err := Login()
-		if err != nil {
-			// nb: sufficient logging from login should have occurred
-			return err
-		}
-
-		// re-load context, including refreshed token
-		cfg = config.GetCurrentContext()
-
-		// retry the request
-		log.Info("Retrying the request with the refreshed token")
-		req, err = prepareJSONRequest(cfg, client, method, path, body, options.Headers)
-		if err != nil {
-			return err // anything that needed logging has been logged
-		}
-		resp, err = client.Do(req)
-		if err != nil {
-			return fmt.Errorf("%v request to %q failed: %v", method, req.RequestURI, err.Error())
-		}
-
-		// log error if it occurred
-		if resp.StatusCode/100 != 2 {
-			// log error before trying to parse body, more processing later
-			log.Errorf("Request failed, status %q; more info to follow", resp.Status)
-		} else {
-			log.Infof("Request completed successfully: %v", resp.Status)
-		}
-
-		// collect response body (whether success or error)
-		defer resp.Body.Close()
-		respBytes, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("Failed reading response to %v to %q: %v", method, req.RequestURI, err)
-		}
-	}
-
-	if resp.StatusCode/100 != 2 {
-		return parseIntoError(resp, respBytes)
-	}
-
-	if method != "DELETE" {
-		if err := json.Unmarshal(respBytes, out); err != nil {
-			return fmt.Errorf("Failed to JSON parse the response: %v (%q)", err, respBytes)
-		}
-		//log.Infof("API Response as struct %+v\n", out) //@@
-	}
-
-	// return response headers
-	if options != nil {
-		if resp.Header != nil {
-			options.ResponseHeaders = map[string][]string(resp.Header)
-		} else {
-			options.ResponseHeaders = nil
-		}
-	}
-
-	return nil
-}
-
-func prepareJSONRequest(cfg *config.Context, client *http.Client, method string, path string, body any, headers map[string]string) (*http.Request, error) {
-	// marshal body data into a io.Reader
+	// prepare a body reader
 	var bodyReader io.Reader = nil
-	if body != nil {
+	if jsonify {
+		// marshal body data to JSON
 		bodyBytes, err := json.Marshal(body)
 		if err != nil {
-			log.Errorf("Failed to marshal data: %v", err.Error())
-			return nil, err
+			return nil, fmt.Errorf("Failed to marshal body data: %w", err)
 		}
-		//log.Infof("HTTP Request body: %q", bodyBytes)
+		bodyReader = bytes.NewReader(bodyBytes)
+	} else if body != nil {
+		// provide body data as a io.Reader
+		bodyBytes, ok := body.([]byte)
+		if !ok {
+			return nil, fmt.Errorf("(bug) HTTP request body type must be []byte if Content-Type is provided, found %T instead", body)
+		}
 		bodyReader = bytes.NewReader(bodyBytes)
 	}
 
-	// create a HTTP request
+	// create HTTP request
+	path, query, _ := strings.Cut(path, "?")
 	url, err := url.Parse(cfg.URL)
 	if err != nil {
 		log.Fatalf("Failed to parse the url provided in context (%q): %v", cfg.URL, err)
 	}
 	url.Path = path
-
-	// checking if there is a query in the path
-	iQuery := strings.Index(path, "?")
-
-	if iQuery > -1 {
-		// extracting the query and updating the path
-
-		purePath := path[:iQuery]
-		query := path[iQuery+1:]
-		url.RawQuery = query
-		url.Path = purePath
-	}
-
+	url.RawQuery = query
 	req, err := http.NewRequest(method, url.String(), bodyReader)
 	if err != nil {
-		log.Errorf("Failed to create a request %q: %v", url.String(), err.Error())
-		return nil, err
+		return nil, fmt.Errorf("Failed to create a request for %q: %w", url.String(), err)
 	}
 
-	// add headers
-	req.Header.Add("Accept", "application/json")
-
-	// set Content-Type in case it hasn't been provided by the caller
-
-	if headers["Content-Type"] == "" {
+	// add headers that are not already provided
+	if jsonify {
 		contentType := "application/json"
 		if method == "PATCH" {
 			contentType = "application/merge-patch+json"
 		}
 		req.Header.Add("Content-Type", contentType)
 	}
+	if headers == nil || headers["Accept"] == "" {
+		req.Header.Add("Accept", "application/json")
+	}
+	if headers == nil || headers["Authorization"] == "" {
+		req.Header.Add("Authorization", "Bearer "+cfg.Token)
+	}
 
-	req.Header.Add("Authorization", "Bearer "+cfg.Token)
-
+	// add explicit headers
 	for k, v := range headers {
 		req.Header.Add(k, v)
 	}
@@ -298,7 +141,7 @@ func prepareJSONRequest(cfg *config.Context, client *http.Client, method string,
 	return req, nil
 }
 
-func httpRequest(method string, path string, body []byte, out any, options *Options) error {
+func httpRequest(method string, path string, body any, out any, options *Options) error {
 	log.WithFields(log.Fields{"method": method, "path": path}).Info("Calling FSO platform API")
 
 	// create a default options to avoid nil-checking
@@ -331,13 +174,13 @@ func httpRequest(method string, path string, body []byte, out any, options *Opti
 	// build HTTP request
 	req, err := prepareHTTPRequest(cfg, client, method, path, body, options.Headers)
 	if err != nil {
-		return err // anything that needed logging has been logged
+		return err // assume error messages provide sufficient info
 	}
 
 	// execute request
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("%v request to %q failed: %v", method, req.RequestURI, err)
+		return fmt.Errorf("%v request to %q failed: %w", method, req.RequestURI, err)
 	}
 
 	// log error if it occurred
@@ -351,7 +194,7 @@ func httpRequest(method string, path string, body []byte, out any, options *Opti
 	defer resp.Body.Close()
 	respBytes, err = io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("Failed reading response to %v to %q: %v", method, req.RequestURI, err)
+		return fmt.Errorf("Failed reading response to %v to %q: %w", method, req.RequestURI, err)
 	}
 
 	// handle special case when access token needs to be refreshed and request retried
@@ -359,8 +202,7 @@ func httpRequest(method string, path string, body []byte, out any, options *Opti
 		log.Info("Current token is no longer valid; trying to refresh")
 		err := Login()
 		if err != nil {
-			// nb: sufficient logging from login should have occurred
-			return err
+			return fmt.Errorf("Failed to login: %w", err)
 		}
 
 		// re-load context, including refreshed token
@@ -370,11 +212,11 @@ func httpRequest(method string, path string, body []byte, out any, options *Opti
 		log.Info("Retrying the request with the refreshed token")
 		req, err = prepareHTTPRequest(cfg, client, method, path, body, options.Headers)
 		if err != nil {
-			return err // anything that needed logging has been logged
+			return err // error should have enough context
 		}
 		resp, err = client.Do(req)
 		if err != nil {
-			return fmt.Errorf("%v request to %q failed: %v", method, req.RequestURI, err.Error())
+			return fmt.Errorf("%v request to %q failed: %w", method, req.RequestURI, err)
 		}
 
 		// log error if it occurred
@@ -389,7 +231,7 @@ func httpRequest(method string, path string, body []byte, out any, options *Opti
 		defer resp.Body.Close()
 		respBytes, err = io.ReadAll(resp.Body)
 		if err != nil {
-			return fmt.Errorf("Failed reading response to %v to %q: %v", method, req.RequestURI, err)
+			return fmt.Errorf("Failed reading response to %v to %q: %w", method, req.RequestURI, err)
 		}
 	}
 
@@ -397,94 +239,63 @@ func httpRequest(method string, path string, body []byte, out any, options *Opti
 		return parseIntoError(resp, respBytes)
 	}
 
+	// process body
 	contentType := resp.Header.Get("content-type")
 	if method != "DELETE" {
-		// when command type is not SOLUTION_DOWNLOAD, parse the response to be JSON
-		if contentType != "application/octet-stream" && contentType != "application/zip" {
-			if err := json.Unmarshal(respBytes, out); err != nil {
-				return fmt.Errorf("Failed to JSON parse the response: %v (%q)", err, respBytes)
-			}
-		} else {
+		// for downloaded files, save them
+		if contentType == "application/octet-stream" || contentType == "application/zip" {
 			var solutionFileName = options.Headers["solutionFileName"]
-			// zip the buffer data to a zip with solution name in current directory
+			if solutionFileName == "" {
+				return fmt.Errorf("(bug) filename not provided for response type %q", contentType)
+			}
+
+			// store the response data into specified file
 			err := os.WriteFile(solutionFileName, respBytes, 0777)
 			if err != nil {
-				log.Fatalf("Failed to parse the Solution download API buffer response: %v (%q)", err, respBytes)
+				return fmt.Errorf("Failed to save the solution archive file as %q: %w", solutionFileName, err)
+			}
+		} else {
+			// unmarshal response from JSON (assuming JSON data, even if the content-type is not set)
+			if err := json.Unmarshal(respBytes, out); err != nil {
+				return fmt.Errorf("Failed to JSON parse the response: %w (%q)", err, respBytes)
 			}
 		}
 	}
+
+	// return response headers
+	if options != nil {
+		if resp.Header != nil {
+			options.ResponseHeaders = map[string][]string(resp.Header)
+		} else {
+			options.ResponseHeaders = nil
+		}
+	}
+
 	return nil
-}
-
-func prepareHTTPRequest(cfg *config.Context, client *http.Client, method string, path string, body []byte, headers map[string]string) (*http.Request, error) {
-	// marshal body data into a io.Reader
-	bodyReader := bytes.NewReader(body)
-
-	// if body != nil {
-	// 	bodyBytes, err := json.Marshal(body)
-	// 	if err != nil {
-	// 		log.Errorf("Failed to marshal data: %v", err.Error())
-	// 		return nil, err
-	// 	}
-	// 	//log.Infof("HTTP Request body: %q", bodyBytes)
-	// 	bodyReader = bytes.NewReader(bodyBytes)
-	// }
-
-	// create a HTTP request
-
-	url, err := url.Parse(cfg.URL)
-	if err != nil {
-		log.Fatalf("Failed to parse the url provided in context (%q): %v", cfg.URL, err)
-	}
-	url.Path = path
-
-	// checking if there is a query in the path
-	iQuery := strings.Index(path, "?")
-
-	if iQuery > -1 {
-		// extracting the query and updating the path
-
-		purePath := path[:iQuery]
-		query := path[iQuery+1:]
-		url.RawQuery = query
-		url.Path = purePath
-	}
-
-	req, err := http.NewRequest(method, url.String(), bodyReader)
-	if err != nil {
-		log.Errorf("Failed to create a request %q: %v", url.String(), err.Error())
-		return nil, err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+cfg.Token)
-
-	for k, v := range headers {
-		req.Header.Add(k, v)
-	}
-
-	return req, nil
 }
 
 // parseError creates an error from HTTP response data
 // method creates either an error with wrapped response body
 // or a Problem struct in case the response is of type "application/problem+json"
 func parseIntoError(resp *http.Response, respBytes []byte) error {
+	// try various strategies for humanizing the error output, from the most
+	// specific to the generic
 
-	// attempt to parse as standardised JSON object
-	// on failure continue with the other strategy
-	if resp.Header.Get("Content-Type") == "application/problem+json" {
-		var problem Problem
-		err := json.Unmarshal(respBytes, &problem)
-		if err == nil {
-			return problem
-		}
+	// try as "Content-Type: application/problem+json", even if
+	// the content type is not set this way (some APIs don't set it)
+	var problem Problem
+	err := json.Unmarshal(respBytes, &problem)
+	if err == nil {
+		return problem
 	}
+
+	// attempt to parse response as a generic JSON object
 	var errobj any
-	// attempt to parse response as JSON object
-	err := json.Unmarshal(respBytes, &errobj)
-	if err != nil {
-		// process as a string instead, ignore parsing error
-		errobj = bytes.NewBuffer(respBytes).String()
+	err = json.Unmarshal(respBytes, &errobj)
+	if err == nil {
+		return fmt.Errorf("error response: %+v", errobj)
 	}
-	return fmt.Errorf("error response: %+v", errobj)
+
+	// fallback to string
+	return fmt.Errorf("error response: %v", bytes.NewBuffer(respBytes).String())
 }
