a7df71a4551226f0594c536ed6b3929922b0607c
Renato Quedas <rquedas@cisco.com>
2023-03-23 06:28:01 -0500

    fsoc melt refactor to support melt library

--- cmd/melt/push.go
+++ cmd/melt/push.go
@@ -2,7 +2,9 @@ package melt
 
 import (
 	"io"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/apex/log"
 	"github.com/spf13/cobra"
@@ -33,92 +35,49 @@ func getMeltPushCmd() *cobra.Command {
 
 func meltSend(cmd *cobra.Command, args []string) {
 	if len(args) < 1 {
-		log.Error("Missing the fsoc telemetry data model .yaml file name")
+		log.Error("Missing the fsoc telemetry data model .yaml file name!s")
+		return
+	}
+	if !cmd.Flags().Changed("profile") {
+		log.Error("The required --profile <agent-principal-profile> flag is missing!")
 		return
 	}
 	dataFileName := args[0]
 	sendDataFromFile(cmd, dataFileName)
 }
 
-// func sendDataFromFile(cmd *cobra.Command, dataFileName string) {
-// 	fsoData, err := loadDataFile(dataFileName)
-// 	if err != nil {
-// 		log.Fatalf("Can't open the file named %q: %v", dataFileName, err)
-// 	}
-// 	cfg := config.GetCurrentContext()
-// 	authObj, err := api.AgentPrincipalLogin(cfg, fsoData.Credentials)
-// 	if err != nil {
-// 		log.Fatalf("Couldn't authenticate using agent principal credentials: %v", err)
-// 	}
-
-// 	// authObj := &api.TokenStruct{
-// 	// 	AccessToken: "thisIsTheToken",
-// 	// }
-// 	exportMeltStraight(cmd, authObj, fsoData)
-// }
-
 func sendDataFromFile(cmd *cobra.Command, dataFileName string) {
 	fsoData, err := loadDataFile(dataFileName)
 	if err != nil {
 		log.Fatalf("Can't open the file named %q: %v", dataFileName, err)
 	}
+
+	for _, entity := range fsoData.Melt {
+		entity.SetAttribute("telemetry.sdk.name", "fsoc-melt")
+		for _, m := range entity.Metrics {
+			et := time.Now()
+			if len(m.DataPoints) == 0 {
+				for i := 1; i < 6; i++ {
+					st := et.Add(time.Minute * -1)
+					et = st
+					m.AddDataPoint(st.UnixNano(), et.UnixNano(), rand.Float64()*50)
+				}
+			}
+		}
+		for _, l := range entity.Logs {
+			if l.Timestamp == 0 {
+				l.Timestamp = time.Now().UnixNano()
+			}
+		}
+	}
+
 	exportMeltStraight(cmd, fsoData)
 }
-
-// func exportMeltStraight(cmd *cobra.Command, authObj *api.TokenStruct, fsoData *melt.FsocData) {
-
-// 	token := authObj.AccessToken
-// 	endPoint := "/data/v1"
-
-// 	output.PrintCmdStatus(cmd, "Generating new MELT telemetry... \n")
-// 	exportMelt(cmd, endPoint, token, *fsoData)
-// }
 
 func exportMeltStraight(cmd *cobra.Command, fsoData *melt.FsocData) {
 	output.PrintCmdStatus(cmd, "Generating new MELT telemetry... \n")
 	exportMelt(cmd, *fsoData)
 }
-
-// func exportMeltTicker(cmd *cobra.Command, fsoData *melt.FsoData) {
-// 	interval, _ := time.ParseDuration("1m")
-// 	ticker := time.NewTicker(interval)
-
-// 	for _ = range ticker.C {
-
-// 		authObj, err := login.Login(fsoData.Credentials)
-// 		if err != nil {
-// 			log.Fatalf("Failed to authenticate %v", err.Error())
-// 			return
-// 		}
-
-// 		token := authObj.AccessToken
-// 		urlStruct, err := url.Parse(fsoData.Credentials.TokenURL)
-// 		server := urlStruct.Host
-
-// 		endPoint := "https://" + server + "/data/v1"
-
-// 		output.PrintCmdStatus("Generating and pushing OTLP MELT Telemetry data to the FSO Platform...")
-// 		exportMelt(cmd, endPoint, token, *fsoData)
-// 	}
-// }
-
-// func exportMelt(cmd *cobra.Command, endPoint, token string, fsoData melt.FsocData) {
-// 	// invoke the exporter
-// 	exp := &melt.Exporter{}
-
-// 	output.PrintCmdStatus(cmd, "\nExporting metrics... \n")
-// 	err := exp.ExportMetrics(fsoData.Melt)
-// 	if err != nil {
-// 		log.Fatalf("Error exporting metrics: %s", err)
-// 	}
-
-// 	output.PrintCmdStatus(cmd, "\nExporting logs... \n")
-// 	err = exp.ExportLogs(fsoData.Melt)
-// 	if err != nil {
-// 		log.Fatalf("Error exporting logs: %s", err)
-// 	}
-
-// }
 
 func exportMelt(cmd *cobra.Command, fsoData melt.FsocData) {
 	// invoke the exporter
@@ -157,31 +116,3 @@ func loadDataFile(fileName string) (*melt.FsocData, error) {
 
 	return fsoData, nil
 }
-
-// func exportEvents(ctx context.Context, endPoint, token string) error {
-// 	el := []*melt.Entity{}
-
-// 	// create an entity
-// 	e1 := melt.NewEntity("geometry:square")
-// 	e1.SetAttribute("geometry.shape.name", "Square 100")
-// 	e1.SetAttribute("geometry.shape.type", "square")
-// 	e1.SetAttribute("geometry.square.side", "10")
-// 	e1.SetAttribute("telemetry.sdk.name", "appd-datagen")
-
-// 	// add events to the entity
-// 	for i := 1; i < 5; i++ {
-// 		l := melt.NewEvent("geometry:operation")
-// 		l.SetAttribute("type", "draw")
-// 		l.Timestamp = time.Now().UnixNano()
-// 		e1.AddLog(l)
-// 	}
-// 	el = append(el, e1)
-
-// 	// invoke the exporter
-// 	exp := &exporter.Exporter{}
-// 	expReq := melt.ExportRequest{
-// 		EndPoint:    endPoint,
-// 		Credentials: melt.Credentials{Token: token},
-// 	}
-// 	return exp.ExportLogs(ctx, expReq, el)
-// }
