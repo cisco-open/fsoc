e4399417c1abac70e70c1a13b0ba7f862ff9138d
Sergey Sergeev <sergey.sergeev@appdynamics.com>
2023-03-25 21:04:05 -0700

    refactoring code, adding more comments

--- cmd/openai/chat.go
+++ cmd/openai/chat.go
@@ -15,82 +15,56 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Initialize chat command and add it to the root command
 func init() {
-	// Add the chat command to the root command
+	chatCmd.Flags().String("model", "", "Specify the model to use for the chat (default: "+openai.GPT3Dot5Turbo+")")
 }
 
+// Chat is a struct representing a single chat conversation, which contains an array of ChatCompletionMessage.
+type Chat struct {
+	Messages []openai.ChatCompletionMessage
+}
+
+// chatCmd defines the chat command for interacting with ChatGPT.
 var chatCmd = &cobra.Command{
 	Use:   "chat",
 	Short: "Interact with ChatGPT",
-	Long:  `This command allows you to interact with ChatGPT by sending messages.`,
+	Long: `Interact with ChatGPT by sending messages. You can send text input and receive AI-generated responses.
+	Type 'exit' to stop the conversation or 'switch <chatID>' to switch to a different chat.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Get the API key from the environment variable or config file
-		apiKey := os.Getenv("OPENAI_API_KEY")
-		if apiKey == "" {
-			cfg := config.GetCurrentContext()
-			apiKey = cfg.OpenAIApiKey
-		}
+		apiKey := getAPIKey()
+
+		// Get the selected model from the flag
+		model := getModel(cmd)
 
 		// Initialize the OpenAI client
 		client := openai.NewClient(apiKey)
 
 		// Initialize the chatID and chatConversations map
 		chatID := newChatID()
-		chatConversations := initChatConversations()
+		_ = initChatConversations()
+
+		// Display the initial chat session information
+		displayChatSessionInfo(chatID)
 
 		// Set the input message
 		reader := bufio.NewReader(os.Stdin)
 
-		fmt.Println("You are now in chat ID:", chatID)
-		fmt.Println("Type 'exit' to stop the conversation or 'switch <chatID>' to switch to a different chat.")
-
 		for {
 			// Read input from the user
-			fmt.Print("> ")
-			inputMessage, _ := reader.ReadString('\n')
-			inputMessage = strings.TrimSpace(inputMessage)
+			inputMessage := readUserInput(reader)
 
-			if inputMessage == "exit" {
-				break
-			}
-
-			if strings.HasPrefix(inputMessage, "switch ") {
-				newID := strings.TrimSpace(strings.TrimPrefix(inputMessage, "switch"))
-				if _, exists := chatConversations[newID]; exists {
-					chatID = newID
-					fmt.Println("You have switched to chat ID:", chatID)
-				} else {
-					fmt.Println("Chat ID not found.")
-				}
+			// Handle exit and switch commands
+			if exitOrSwitchCommand(inputMessage, &chatID) {
 				continue
 			}
 
-			// Add this block to handle commands within backticks
-			regex := regexp.MustCompile("`([^`]*)`")
-			inputMessage = regex.ReplaceAllStringFunc(inputMessage, func(matched string) string {
-				shellCommand := strings.Trim(matched, "`")
-				cmdOutput, err := runShellCommand(shellCommand)
-				if err != nil {
-					fmt.Printf("Error executing command '%s': %v\n", shellCommand, err)
-					return "<error executing command>"
-				}
-				return cmdOutput
-			})
+			// Replace shell commands within backticks with their output
+			inputMessage = replaceShellCommands(inputMessage)
 
-			// Add the input message to the conversation
-			_ = continueChat(chatID, openai.ChatCompletionMessage{
-				Role:    openai.ChatMessageRoleUser,
-				Content: inputMessage,
-			})
-
-			// Send the message to ChatGPT
-			resp, err := client.CreateChatCompletion(
-				context.Background(),
-				openai.ChatCompletionRequest{
-					Model:    openai.GPT3Dot5Turbo,
-					Messages: chatConversations[chatID].Messages,
-				},
-			)
+			// Add the input message to the conversation and send the message to ChatGPT
+			responseMessage, err := processUserInput(client, model, chatID, inputMessage)
 
 			// Check for errors and print the response
 			if err != nil {
@@ -98,7 +72,6 @@ var chatCmd = &cobra.Command{
 				return
 			}
 
-			responseMessage := resp.Choices[0].Message.Content
 			fmt.Printf("Assistant: %s\n", responseMessage)
 
 			// Add the response message to the conversation
@@ -108,11 +81,6 @@ var chatCmd = &cobra.Command{
 			})
 		}
 	},
-}
-
-// Chat is a struct representing a single chat conversation, which contains an array of ChatCompletionMessage.
-type Chat struct {
-	Messages []openai.ChatCompletionMessage
 }
 
 // chatConversations is a map that stores multiple chat conversations by chatID.
@@ -132,6 +100,110 @@ func initChatConversations() map[string]*Chat {
 	return chatConversations
 }
 
+// runShellCommand executes the given shell command and returns its output as a string.
+// In case of an error, it returns an empty string and the error.
+func runShellCommand(command string) (string, error) {
+	cmd := exec.Command("sh", "-c", command)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
+}
+
+// getAPIKey retrieves the API key from the environment variable or config file.
+func getAPIKey() string {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		cfg := config.GetCurrentContext()
+		apiKey = cfg.OpenAIApiKey
+	}
+	return apiKey
+}
+
+// getModel retrieves the selected model from the flag or uses the default.
+func getModel(cmd *cobra.Command) string {
+	model, _ := cmd.Flags().GetString("model")
+	if model == "" {
+		model = openai.GPT3Dot5Turbo
+	}
+	return model
+}
+
+// displayChatSessionInfo displays the initial chat session information.
+func displayChatSessionInfo(chatID string) {
+	fmt.Println("You are now in chat ID:", chatID)
+	fmt.Println("Type 'exit' to stop the conversation or 'switch <chatID>' to switch to a different chat.")
+}
+
+// readUserInput reads the input from the user.
+func readUserInput(reader *bufio.Reader) string {
+	fmt.Print("> ")
+	inputMessage, _ := reader.ReadString('\n')
+	inputMessage = strings.TrimSpace(inputMessage)
+	return inputMessage
+}
+
+// exitOrSwitchCommand handles exit and switch commands, returning true if either is executed.
+func exitOrSwitchCommand(inputMessage string, chatID *string) bool {
+	if inputMessage == "exit" {
+		os.Exit(0)
+	}
+
+	if strings.HasPrefix(inputMessage, "switch ") {
+		newID := strings.TrimSpace(strings.TrimPrefix(inputMessage, "switch"))
+		if _, exists := chatConversations[newID]; exists {
+			*chatID = newID
+			fmt.Println("You have switched to chat ID:", *chatID)
+		} else {
+			fmt.Println("Chat ID not found.")
+		}
+		return true
+	}
+
+	return false
+}
+
+// replaceShellCommands replaces shell commands within backticks with their output.
+func replaceShellCommands(inputMessage string) string {
+	regex := regexp.MustCompile("`([^`]*)`")
+	inputMessage = regex.ReplaceAllStringFunc(inputMessage, func(matched string) string {
+		shellCommand := strings.Trim(matched, "`")
+		cmdOutput, err := runShellCommand(shellCommand)
+		if err != nil {
+			fmt.Printf("Error executing command '%s': %v\n", shellCommand, err)
+			return "<error executing command>"
+		}
+		return cmdOutput
+	})
+	return inputMessage
+}
+
+// processUserInput adds the input message to the conversation and sends the message to ChatGPT.
+func processUserInput(client *openai.Client, model string, chatID string, inputMessage string) (string, error) {
+	_ = continueChat(chatID, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: inputMessage,
+	})
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model:    model,
+			Messages: chatConversations[chatID].Messages,
+		},
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	responseMessage := resp.Choices[0].Message.Content
+	return responseMessage, nil
+}
+
 // continueChat appends a new message to an existing chat conversation identified by chatID.
 // If the chatID doesn't exist in the chatConversations map, a new conversation is created.
 // The function takes chatID and message as input parameters.
@@ -144,18 +216,7 @@ func continueChat(chatID string, message openai.ChatCompletionMessage) error {
 	return nil
 }
 
-// Add the runShellCommand function here
-func runShellCommand(command string) (string, error) {
-	cmd := exec.Command("sh", "-c", command)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-	return out.String(), nil
-}
-
+// NewChatCmd returns the chatCmd to be used by the main application.
 func NewChatCmd() *cobra.Command {
 	return chatCmd
 }
