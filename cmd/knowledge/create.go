6b29a9842a6c077f1815411d5b5b5ac9542ab9c2
Jack Morgan <jack.morgan@appdynamics.com>
2023-04-10 17:10:23 -0700

    FSOC-141: change objstore command syntax to knowledge

---
+++ cmd/knowledge/create.go
@@ -0,0 +1,182 @@
+// Copyright 2022 Cisco Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package knowledge
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/apex/log"
+	"github.com/spf13/cobra"
+
+	"github.com/cisco-open/fsoc/output"
+	"github.com/cisco-open/fsoc/platform/api"
+)
+
+var objStoreInsertCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new object of a given type",
+	Long: `This command allows the creation of a new object of a given type in the Object Store.
+
+Example:
+  fsoc knowledge create --type<fully-qualified-typename> --object-file=<fully-qualified-path> --layer-type=<valid-layer-type> [--layer-id=<valid-layer-id>]
+`,
+
+	Args:             cobra.ExactArgs(0),
+	Run:              insertObject,
+	TraverseChildren: true,
+}
+
+func getCreateObjectCmd() *cobra.Command {
+	objStoreInsertCmd.Flags().
+		String("type", "", "The fully qualified type name of the object")
+	_ = objStoreInsertCmd.MarkPersistentFlagRequired("type")
+
+	objStoreInsertCmd.Flags().
+		String("object-file", "", "The fully qualified path to the json file containing the object data")
+	_ = objStoreInsertCmd.MarkPersistentFlagRequired("objectFile")
+
+	objStoreInsertCmd.Flags().
+		String("layer-type", "", "The layer-type that the created object will be added to")
+	_ = objStoreInsertCmd.MarkPersistentFlagRequired("layer-type")
+
+	objStoreInsertCmd.Flags().
+		String("layer-id", "", "The layer-id that the created object will be added to. Optional for TENANT and SOLUTION layers ")
+
+	return objStoreInsertCmd
+
+}
+
+func insertObject(cmd *cobra.Command, args []string) {
+	objType, _ := cmd.Flags().GetString("type")
+
+	objJsonFilePath, _ := cmd.Flags().GetString("object-file")
+	objectFile, err := os.Open(objJsonFilePath)
+	if err != nil {
+		log.Fatalf("Can't find the object definition file named %q", objJsonFilePath)
+	}
+	defer objectFile.Close()
+
+	objectBytes, _ := io.ReadAll(objectFile)
+	var objectStruct map[string]interface{}
+	err = json.Unmarshal(objectBytes, &objectStruct)
+	if err != nil {
+		log.Fatalf("Failed to parse object data from file %q: %v. Make sure the object definition has all the required field and is valid according to the type definition.", objJsonFilePath, err)
+	}
+
+	layerType, _ := cmd.Flags().GetString("layer-type")
+	layerID := getCorrectLayerID(layerType, objType)
+
+	if layerID == "" {
+		if !cmd.Flags().Changed("layer-id") {
+			log.Fatal("Unable to set layer-id flag from given context. Please specify a unique layer-id value with the --layer-id flag")
+		}
+		layerID, err = cmd.Flags().GetString("layer-id")
+		if err != nil {
+			log.Fatalf("error trying to get %q flag value: %w", "layer-id", err)
+		}
+	}
+
+	headers := map[string]string{
+		"layer-type": layerType,
+		"layer-id":   layerID,
+	}
+
+	var res any
+	// objJsonStr, err := json.Marshal(objectStruct)
+	err = api.JSONPost(getObjStoreObjectUrl()+"/"+objType, objectStruct, &res, &api.Options{Headers: headers})
+	if err != nil {
+		log.Fatalf("Failed to create object: %v", err)
+	} else {
+		log.Infof("Successfully created a %q object", objType)
+	}
+}
+
+func getObjStoreObjectUrl() string {
+	return "objstore/v1beta/objects"
+}
+
+var objStoreInsertPatchedObjectCmd = &cobra.Command{
+	Use:   "create-patch",
+	Short: "Create a new patched object of a given type",
+	Long: `This command allows the creation of a new patched object of a given type in the Object Store.
+A patched object inherits values from an object that exists at a higher layer and can also override mutable fields when needed.
+
+Example:
+  fsoc knowledge create-patch --type<fully-qualified-typename> --object-file=<fully-qualified-path> --target-layer-type=<valid-layer-type> --target-object-id=<valid-object-id>`,
+
+	Args:             cobra.ExactArgs(0),
+	Run:              insertPatchObject,
+	TraverseChildren: true,
+}
+
+func getCreatePatchObjectCmd() *cobra.Command {
+	objStoreInsertPatchedObjectCmd.Flags().
+		String("type", "", "The fully qualified type name of the object")
+	_ = objStoreInsertPatchedObjectCmd.MarkPersistentFlagRequired("type")
+
+	objStoreInsertPatchedObjectCmd.Flags().
+		String("target-object-id", "", "The id of the object for which you want to create a patched object at a lower layer")
+	_ = objStoreInsertPatchedObjectCmd.MarkPersistentFlagRequired("target-object-id")
+
+	objStoreInsertPatchedObjectCmd.Flags().
+		String("object-file", "", "The fully qualified path to the json file containing the object definition")
+	_ = objStoreInsertPatchedObjectCmd.MarkPersistentFlagRequired("objectFile")
+
+	objStoreInsertPatchedObjectCmd.Flags().
+		String("target-layer-type", "", "The layer-type at which the patch object will be created. For inheritance purposes, this should always be a `lower` layer than the target object's layer")
+	_ = objStoreInsertPatchedObjectCmd.MarkPersistentFlagRequired("target-layer-type")
+
+	return objStoreInsertPatchedObjectCmd
+}
+
+func insertPatchObject(cmd *cobra.Command, args []string) {
+	objType, _ := cmd.Flags().GetString("type")
+	parentObjId, _ := cmd.Flags().GetString("target-object-id")
+
+	objJsonFilePath, _ := cmd.Flags().GetString("object-file")
+	objectFile, err := os.Open(objJsonFilePath)
+	if err != nil {
+		log.Fatalf("Can't find the object definition file %q", objJsonFilePath)
+		return
+	}
+	defer objectFile.Close()
+
+	objectBytes, _ := io.ReadAll(objectFile)
+	var objectStruct map[string]interface{}
+	err = json.Unmarshal(objectBytes, &objectStruct)
+	if err != nil {
+		log.Fatalf("Failed to parse object data from file %q: %v. Make sure the object definition has all the required fields and is valid according to the type definition.", objJsonFilePath, err)
+	}
+
+	layerType, _ := cmd.Flags().GetString("target-layer-type")
+	layerID := getCorrectLayerID(layerType, objType)
+
+	headers := map[string]string{
+		"layer-type": layerType,
+		"layer-id":   layerID,
+	}
+
+	var res any
+	err = api.JSONPatch(getObjStoreObjectUrl()+"/"+objType+"/"+parentObjId, objectStruct, &res, &api.Options{Headers: headers})
+	if err != nil {
+		log.Fatalf("Failed to create object: %v", err)
+		return
+	} else {
+		output.PrintCmdOutput(cmd, fmt.Sprintf("Successfully created a patched %q object at the %s layer.\n", objType, layerType))
+	}
+}
---
+++ cmd/knowledge/update.go
@@ -0,0 +1,124 @@
+// Copyright 2022 Cisco Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package knowledge
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/apex/log"
+	"github.com/spf13/cobra"
+
+	"github.com/cisco-open/fsoc/output"
+	"github.com/cisco-open/fsoc/platform/api"
+)
+
+var objStoreUpdateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update an existent knowledge object",
+	Long: `This command allows the an existent knowledge object to be updated according to the fields and values provided in a .json file.
+
+	Usage:
+	fsoc knowledge update --type=<fully-qualified-typename> 
+	--object-id=<object id>
+	--object-file=<fully-qualified-path> 
+	--layer-type=[SOLUTION|ACCOUNT|GLOBALUSER|TENANT|LOCALUSER]
+	--layer-id=<respective-layer-id>
+	
+	Flags/Options:
+	--type - Flag to indicate the fully qualified type name of the object that you would like to update
+	--object-id - Flag to indicate the ID of the object that you want to update
+	--object-file - Flag to indicate the fully qualified path (from your root directory) to the file containing the definition of the object that you want to update. Please note that update internally calls HTTP PUT so you will need to specify all fields in the object (even if you are updating just one field)
+	--layer-type - Flag to indicate the layer at which the object you would like to update exists
+	--layer-id - OPTIONAL Flag to specify a custom layer ID for the object that you would like to update.  This is calculated automatically for all layers currently supported but can be overridden with this flag`,
+
+	Args:             cobra.ExactArgs(0),
+	Run:              updateObject,
+	TraverseChildren: true,
+}
+
+func getUpdateObjectCmd() *cobra.Command {
+	objStoreUpdateCmd.Flags().
+		String("type", "", "The fully qualified type name of the object")
+	_ = objStoreUpdateCmd.MarkPersistentFlagRequired("type")
+
+	objStoreUpdateCmd.Flags().
+		String("object-id", "", "The id of the knowledge object been updated")
+	_ = objStoreUpdateCmd.MarkPersistentFlagRequired("type")
+
+	objStoreUpdateCmd.Flags().
+		String("object-file", "", "The fully qualified path to the json file containing the knowledge object data definition")
+	_ = objStoreUpdateCmd.MarkPersistentFlagRequired("objectFile")
+
+	objStoreUpdateCmd.Flags().
+		String("layer-type", "", "The layer-type of the updated object")
+	_ = objStoreUpdateCmd.MarkPersistentFlagRequired("layer-type")
+
+	objStoreUpdateCmd.Flags().
+		String("layer-id", "", "The layer-id of the updated object. Optional for TENANT and SOLUTION layers ")
+
+	return objStoreUpdateCmd
+
+}
+
+func updateObject(cmd *cobra.Command, args []string) {
+	objType, _ := cmd.Flags().GetString("type")
+
+	objJsonFilePath, _ := cmd.Flags().GetString("object-file")
+	objectFile, err := os.Open(objJsonFilePath)
+	if err != nil {
+		log.Fatalf("Can't find the object definition file named %s", objJsonFilePath)
+	}
+	defer objectFile.Close()
+
+	objectBytes, _ := io.ReadAll(objectFile)
+	var objectStruct map[string]interface{}
+	err = json.Unmarshal(objectBytes, &objectStruct)
+	if err != nil {
+		log.Fatalf("Can't parse file %q. Make sure the object definition has all the required field and is valid according to the type definition.", objJsonFilePath)
+	}
+
+	layerType, _ := cmd.Flags().GetString("layer-type")
+	layerID := getCorrectLayerID(layerType, objType)
+
+	if layerID == "" {
+		if !cmd.Flags().Changed("layer-id") {
+			log.Fatal("Unable to set layer-id flag from given context. Please specify a unique layer-id value with the --layer-id flag")
+		}
+		layerID, err = cmd.Flags().GetString("layer-id")
+		if err != nil {
+			log.Fatalf("error trying to get %q flag value: %w", "layer-id", err)
+		}
+	}
+
+	headers := map[string]string{
+		"layer-type": layerType,
+		"layer-id":   layerID,
+	}
+
+	var res any
+	objId, _ := cmd.Flags().GetString("object-id")
+	urlStrf := getObjStoreObjectUrl() + "/%s/%s"
+	objectUrl := fmt.Sprintf(urlStrf, objType, objId)
+
+	output.PrintCmdStatus(cmd, fmt.Sprintf("Replacing object %q with the new data from %q \n", objId, objJsonFilePath))
+	err = api.JSONPut(objectUrl, objectStruct, &res, &api.Options{Headers: headers})
+	if err != nil {
+		log.Fatalf("Object update failed: %v", err)
+	}
+	output.PrintCmdStatus(cmd, "Object updated successfully.\n")
+}
