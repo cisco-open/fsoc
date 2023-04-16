aa6c02e9459120e646e1274f718f13f38404cb6f
pnickolov <pnickolov@users.noreply.github.com>
2023-04-15 21:02:21 -0700

    Add shell autocompletion for --profile (#79)
    * Add shell autocompletion for --profile
    
    ---------
    
    Co-authored-by: Pavel Georgiev <p3l@appdynamics.com>

--- cmd/root.go
+++ cmd/root.go
@@ -55,7 +55,7 @@ Examples:
   fsoc solution list
   fsoc solution list -o json
 
-For more information, see https://github.com/cisco-open/fsoc 
+For more information, see https://github.com/cisco-open/fsoc
 
 NOTE: fsoc is in alpha; breaking changes may occur`,
 	PersistentPreRun:  preExecHook,
@@ -86,6 +86,14 @@ func init() {
 	rootCmd.SetOut(os.Stdout)
 	rootCmd.SetErr(os.Stderr)
 	rootCmd.SetIn(os.Stdin)
+
+	err := rootCmd.RegisterFlagCompletionFunc("profile",
+		func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return config.ListContexts(toComplete), cobra.ShellCompDirectiveDefault
+		})
+	if err != nil {
+		log.Warnf("(likely bug) Failed to register completion function for --profile: %v", err)
+	}
 }
 
 // initConfig reads in config file and ENV variables if set.
