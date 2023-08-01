f4c18aa4d5f01198cf52b40bf2fc4ce851a0cbbc
Linkous Sharp <linkous@appdynamics.com>
2023-08-01 17:45:25 -0500

    Implement recommendations command - added nil pointer check in extractEventsData to prevent panics

--- cmd/optimize/events.go
+++ cmd/optimize/events.go
@@ -43,19 +43,24 @@ var progressEvents = []string{
 func init() {
 	// TODO move this logic to optimize root when implementing unit tests
 	optimizeCmd.AddCommand(NewCmdEvents())
+	optimizeCmd.AddCommand(NewCmdRecommendations())
 }
 
 type eventsFlags struct {
-	clusterId       string
-	namespace       string
-	workloadName    string
-	optimizerId     string
+	clusterId    string
+	namespace    string
+	workloadName string
+	optimizerId  string
+	since        string
+	until        string
+	count        int
+	solutionName string
+}
+
+type eventsCmdFlags struct {
+	eventsFlags
 	includeProgress bool
 	events          []string
-	since           string
-	until           string
-	count           int
-	solutionName    string
 }
 
 type eventsRow struct {
@@ -64,7 +69,7 @@ type eventsRow struct {
 }
 
 func NewCmdEvents() *cobra.Command {
-	var flags eventsFlags
+	var flags eventsCmdFlags
 	command := &cobra.Command{
 		Use:   "events",
 		Short: "Retrieve event logs for a given optimization/workload. Useful for monitoring and debug",
@@ -130,7 +135,7 @@ FETCH events(
 {{ end -}}
 `))
 
-func listEvents(flags *eventsFlags) func(*cobra.Command, []string) error {
+func listEvents(flags *eventsCmdFlags) func(*cobra.Command, []string) error {
 	return func(cmd *cobra.Command, args []string) error {
 		// setup query
 		tempVals := eventsTemplateValues{
@@ -154,7 +159,7 @@ func listEvents(flags *eventsFlags) func(*cobra.Command, []string) error {
 		if flags.optimizerId != "" {
 			filterList = append(filterList, fmt.Sprintf("attributes(optimize.optimization.optimizer_id) = %q", flags.optimizerId))
 		} else if flags.namespace != "" || flags.workloadName != "" {
-			optimizerIds, err := listOptimizations(flags)
+			optimizerIds, err := listOptimizations(&flags.eventsFlags)
 			if err != nil {
 				return fmt.Errorf("listOptimizations: %w", err)
 			}
@@ -261,7 +266,204 @@ func listEvents(flags *eventsFlags) func(*cobra.Command, []string) error {
 	}
 }
 
+type recommendationsCmdFlags struct {
+	eventsFlags
+	includeInvalidated bool
+}
+
+func NewCmdRecommendations() *cobra.Command {
+	var flags recommendationsCmdFlags
+	command := &cobra.Command{
+		Use:              "recommendations",
+		Short:            "Retrieve resulting recommendations for a given optimization/workload",
+		Example:          `TODO`,
+		RunE:             listRecommendations(&flags),
+		TraverseChildren: true,
+		Annotations: map[string]string{
+			output.TableFieldsAnnotation:  "OptimizerId: .EventAttributes[\"optimize.optimization.optimizer_id\"], State: .EventAttributes[\"optimize.recommendation.state\"], CPUcores: .EventAttributes[\"optimize.recommendation.settings.cpu\"], MemoryGiB: .EventAttributes[\"optimize.recommendation.settings.memory\"], Timestamp: .Timestamp",
+			output.DetailFieldsAnnotation: "OptimizerId: .EventAttributes[\"optimize.optimization.optimizer_id\"], State: .EventAttributes[\"optimize.recommendation.state\"], CPUcores: .EventAttributes[\"optimize.recommendation.settings.cpu\"], MemoryGiB: .EventAttributes[\"optimize.recommendation.settings.memory\"], Timestamp: .Timestamp, Attributes: .EventAttributes",
+		},
+	}
+
+	command.Flags().StringVarP(&flags.clusterId, "cluster-id", "c", "", "Retrieve recommendations constrained to a specific cluster by its ID")
+	command.Flags().StringVarP(&flags.namespace, "namespace", "n", "", "Retrieve recommendations constrained to a specific namespace by its name")
+	command.Flags().StringVarP(&flags.workloadName, "workload-name", "w", "", "Retrieve recommendations constrained to a specific workload by its name")
+	command.Flags().StringVarP(&flags.optimizerId, "optimizer-id", "i", "", "Retrieve recommendations for a specific optimizer by its ID")
+	command.MarkFlagsMutuallyExclusive("optimizer-id", "cluster-id")
+	command.MarkFlagsMutuallyExclusive("optimizer-id", "namespace")
+	command.MarkFlagsMutuallyExclusive("optimizer-id", "workload-name")
+
+	command.Flags().BoolVarP(&flags.includeInvalidated, "include-invalidated", "", false, "Include recommendations that have not been verified")
+
+	command.Flags().StringVarP(&flags.since, "since", "s", "-52w", "Retrieve recommendations contained in the time interval starting at a relative or exact time.")
+	command.Flags().StringVarP(&flags.until, "until", "u", "", "Retrieve recommendations contained in the time interval ending at a relative or exact time. (default: now)")
+
+	command.Flags().IntVarP(&flags.count, "count", "", 1, "Limit the number of recommendations retrieved to the specified count")
+
+	command.Flags().StringVarP(&flags.solutionName, "solution-name", "", "optimize", "Intended for developer usage, overrides the name of the solution defining the FMM types for reading")
+	if err := command.LocalFlags().MarkHidden("solution-name"); err != nil {
+		log.Warnf("Failed to set recommendations solution-name flag hidden: %v", err)
+	}
+
+	return command
+}
+
+type recommendationsTemplateValues struct {
+	Since              string
+	Until              string
+	IncludeInvalidated bool
+	Filter             string
+	Limits             string
+	SolutionName       string
+}
+
+var recommendationsTemplate = template.Must(template.New("").Parse(`
+{{ with .Since }}SINCE {{ . }}
+{{ end -}}
+{{ with .Until }}UNTIL {{ . }}
+{{ end -}}
+FETCH events(
+		{{- if .IncludeInvalidated }}
+		{{ .SolutionName }}:recommendation_identified,
+		{{ .SolutionName }}:recommendation_invalidated,
+		{{- end }}
+		{{ .SolutionName }}:recommendation_verified
+	)
+	{{ with .Filter }}[{{ . }}]
+	{{ end -}}
+	{attributes, timestamp}
+{{ with .Limits }}LIMITS events.count({{ . }})
+{{ end -}}
+`))
+
+func listRecommendations(flags *recommendationsCmdFlags) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		// setup query
+		tempVals := recommendationsTemplateValues{
+			Since:              flags.since,
+			Until:              flags.until,
+			IncludeInvalidated: flags.includeInvalidated,
+			SolutionName:       flags.solutionName,
+		}
+
+		filterList := make([]string, 0, 2)
+		if flags.clusterId != "" {
+			filterList = append(filterList, fmt.Sprintf("attributes(k8s.cluster.id) = %q", flags.clusterId))
+		}
+		if flags.optimizerId != "" {
+			filterList = append(filterList, fmt.Sprintf("attributes(optimize.optimization.optimizer_id) = %q", flags.optimizerId))
+		} else if flags.namespace != "" || flags.workloadName != "" {
+			optimizerIds, err := listOptimizations(&flags.eventsFlags)
+			if err != nil {
+				return fmt.Errorf("listOptimizations: %w", err)
+			}
+			if len(optimizerIds) < 1 {
+				output.PrintCmdStatus(cmd, "No optimization entities found matching the given criteria\n")
+				return nil
+			}
+			optIdStr := strings.Join(optimizerIds, "\", \"")
+			filterList = append(filterList, fmt.Sprintf("attributes(optimize.optimization.optimizer_id) IN [\"%v\"]", optIdStr))
+		}
+		tempVals.Filter = strings.Join(filterList, " && ")
+
+		if flags.count != -1 {
+			if flags.count > 1000 {
+				return errors.New("Counts higher than 1000 are not supported")
+			}
+			tempVals.Limits = strconv.Itoa(flags.count)
+		}
+
+		var buff bytes.Buffer
+		if err := recommendationsTemplate.Execute(&buff, tempVals); err != nil {
+			return fmt.Errorf("recommendationsTemplate.Execute: %w", err)
+		}
+		query := buff.String()
+
+		// execute query, process results
+		resp, err := uql.ExecuteQuery(&uql.Query{Str: query}, uql.ApiVersion1)
+		if err != nil {
+			return fmt.Errorf("uql.ExecuteQuery: %w", err)
+		}
+		if resp.HasErrors() {
+			log.Error("Execution of recommendations query encountered errors. Returned data may not be complete!")
+			for _, e := range resp.Errors() {
+				log.Errorf("%s: %s", e.Title, e.Detail)
+			}
+		}
+
+		main_data_set := resp.Main()
+		if main_data_set == nil || len(main_data_set.Data) < 1 {
+			output.PrintCmdStatus(cmd, "No recommendation results found for given input\n")
+			return nil
+		}
+		if len(main_data_set.Data[0]) < 1 {
+			return fmt.Errorf("Main dataset %v first row has no columns", main_data_set.Name)
+		}
+
+		data_set, ok := main_data_set.Data[0][0].(*uql.DataSet)
+		if !ok {
+			return fmt.Errorf("Main dataset %v first row first column (type %T) could not be converted to *uql.DataSet", main_data_set.Name, main_data_set.Data[0][0])
+		}
+		recommendationRows, err := extractEventsData(data_set)
+		if err != nil {
+			return fmt.Errorf("extractEventsData: %w", err)
+		}
+
+		// handle pagination
+		_, next_ok := data_set.Links["next"]
+		if flags.count != -1 {
+			// skip pagination if limits provided. Otherwise, we return the full result list (chunked into count per response)
+			// instead of constraining to count
+			next_ok = false
+		}
+		for page := 2; next_ok; page++ {
+			resp, err = uql.ContinueQuery(data_set, "next")
+			if err != nil {
+				return fmt.Errorf("page %v uql.ContinueQuery: %w", page, err)
+			}
+			if resp.HasErrors() {
+				log.Errorf("Continuation of recommendations query (page %v) encountered errors. Returned data may not be complete!", page)
+				for _, e := range resp.Errors() {
+					log.Errorf("%s: %s", e.Title, e.Detail)
+				}
+			}
+			main_data_set := resp.Main()
+			if main_data_set == nil {
+				log.Errorf("Continuation of recommendations query (page %v) has nil main data. Returned data may not be complete!", page)
+				break
+			}
+			if len(main_data_set.Data) < 1 {
+				return fmt.Errorf("Page %v main dataset %v has no rows", page, main_data_set.Name)
+			}
+			if len(main_data_set.Data[0]) < 1 {
+				return fmt.Errorf("Page %v main dataset %v first row has no columns", page, main_data_set.Name)
+			}
+			data_set, ok = main_data_set.Data[0][0].(*uql.DataSet)
+			if !ok {
+				return fmt.Errorf("Page %v main dataset %v first row first column (type %T) could not be converted to *uql.DataSet", page, main_data_set.Name, main_data_set.Data[0][0])
+			}
+
+			newRows, err := extractEventsData(data_set)
+			if err != nil {
+				return fmt.Errorf("page %v extractEventsData: %w", page, err)
+			}
+			recommendationRows = append(recommendationRows, newRows...)
+			_, next_ok = data_set.Links["next"]
+		}
+
+		output.PrintCmdOutput(cmd, struct {
+			Items []eventsRow `json:"items"`
+			Total int         `json:"total"`
+		}{Items: recommendationRows, Total: len(recommendationRows)})
+
+		return nil
+	}
+}
+
 func extractEventsData(dataset *uql.DataSet) ([]eventsRow, error) {
+	if dataset == nil {
+		return []eventsRow{}, nil
+	}
 	resp_data := &dataset.Data
 	results := make([]eventsRow, 0, len(*resp_data))
 
